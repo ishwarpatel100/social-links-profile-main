aa25f62d6f81498dd95ec61ea2f0bd6626ed544d
Ishawrpatel100 <ishwarrpatel26@gmail.com>
2024-08-27 02:02:41 +0530

    complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "hsl(75, 94%, 57%)",
+        grey: {
+          700: "hsl(0, 0%, 20%)",
+          800: "hsl(0, 0%, 12%)",
+          900: "hsl(0, 0%, 8%)"
+        }
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
